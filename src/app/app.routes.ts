62bab89b8de9e3136a1daaf0c43892f1e2691d24
Adriel <addrriel@hotmail.com>
2025-09-29 07:25:23 -0500

    Falta contenido main y más componentes, barra navbar organizar mejor. Footer realizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Main } from './pages/main/main';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Services } from './pages/services/services';
+import { Doctor } from './pages/doctor/doctor';
+import { Paciente } from './pages/paciente/paciente';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: Main },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'services', component: Services },
+    { path: 'doctor', component: Doctor },
+    { path: 'paciente', component: Paciente },
+    { path: '**', redirectTo: '' }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from "./components/footer/footer";
+import { About } from './pages/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
